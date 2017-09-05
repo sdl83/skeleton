380448f234babcb8123de621870711e1bb13764b
Sarah Le Cam <sdl83@cornell.edu>
2017-09-05 14:29:10 -0400

    Adding netid GET

--- src/main/java/controllers/HelloWorldController.java
+++ src/main/java/controllers/HelloWorldController.java
@@ -2,10 +2,6 @@ package controllers;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Context;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpServletRequest;
-import io.dropwizard.jersey.sessions.Session;
 
 // For a Java class to be eligible to receive ANY requests
 // it must be annotated with at least @Path
@@ -17,7 +13,18 @@ public class HelloWorldController {
     // to the @Path defined at the class level
     @GET
     @Path("/hello")
-    public String helloWorld(@Session HttpSession session) {
-        return "Hello World " + session.toString();
+    public String helloWorld() {
+        return "Hello World ";
+    }
+
+    @GET
+    @Path("/netid")
+    public String getNetID() {
+        return "sdl83";
+    }
+
+    @GET
+    public String getNetID2() {
+        return "sdl83";
     }
 }
